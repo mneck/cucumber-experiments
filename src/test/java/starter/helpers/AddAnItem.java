39d847aea4baf7e71ae5cef6214a0d7aec98434a
Matt N <mneckelmann@gmail.com>
2024-05-07 19:58:57 +0200

    Fix test

--- src/test/java/starter/helpers/AddAnItem.java
+++ src/test/java/starter/helpers/AddAnItem.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.Keys;
 public class AddAnItem {
 
     public static Performable withName(String itemName){
-        return Task.where("{0} adds an item with name "+itemName,
+        return Task.where("{0} adds an item with name " + itemName,
                 Enter.theValue(itemName)
                         .into(TodoListPage.ITEM_NAME_FIELD)
                         .thenHit(Keys.ENTER)
